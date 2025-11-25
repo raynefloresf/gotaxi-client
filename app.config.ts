27dfe6b9b3d819a35007748ec72847f14e14d7aa
Luis Rivas <luisfrm_@outlook.com>
2025-11-25 18:34:54 -0400

    implement initial config

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default {
+  expo: {
+    extra: {
+      apiBaseUrl: process.env.API_BASE_URL,
+      apiTimeout: process.env.API_TIMEOUT,
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    },
+  },
+};
